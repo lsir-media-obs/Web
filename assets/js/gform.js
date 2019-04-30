53998e582c7390f2741913498892db5e290e209e
Jay <jeremie.rappaz@epfl.ch>
2019-04-30 16:38:50 +0200

    add form

---
+++ assets/js/gform.js
@@ -0,0 +1,10 @@
+(function($) {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwEFDubiAPHsTnn22WwGyVyg8Zz4yG1dCfMH_FGhP6NmyEVGZQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+})(jQuery);
