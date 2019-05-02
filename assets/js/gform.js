8e8c57a9d5b1c00fcded562df27f351746618dbb
dtsbourg <dtsbourg@gmail.com>
2019-05-02 16:06:19 +0200

    add alert

--- assets/js/gform.js
+++ assets/js/gform.js
@@ -4,7 +4,7 @@
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {console.log('Success!', response); alert("Thanks for registering! We won't spam - we promise - but we'll keep you posted when new work is available!") })
       .catch(error => console.error('Error!', error.message))
   })
 })(jQuery);
